cf7c592ab2cf8693cea2f6bfdffce9036144e367
German <pinncho@gmail.com>
2023-11-28 22:57:20 +0100

    feat: validate fields when user signs up

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,3 +1,5 @@
+const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
 export async function signIn(email: string, password: string) {
   const response = await fetch(`/api/auth/signin`, {
     method: "POST",
@@ -6,7 +8,6 @@ export async function signIn(email: string, password: string) {
       password,
     }),
   });
-  console.log("signIn statusText", response.statusText);
 
   if (response.status === 400) {
     throw new Error("Password is incorrect");
@@ -17,6 +18,13 @@ export async function signIn(email: string, password: string) {
 }
 
 export async function signUp(name: string, email: string, password: string) {
+  if (!name || !email || !password) {
+    throw new Error("Required fields are not provided");
+  } else if (password.length < 6) {
+    throw new Error("Password should be at least 6 characters long");
+  } else if (!EMAIL_REGEX.test(email)) {
+    throw new Error("Email is not valid");
+  }
   const response = await fetch("/api/auth/signup", {
     method: "POST",
     body: JSON.stringify({
