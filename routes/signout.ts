28be9eba5a8b01f088732272f866348cb11a9c38
German <pinncho@gmail.com>
2023-11-28 20:16:46 +0100

    refactor: rename handler params

--- routes/signout.ts
+++ routes/signout.ts
@@ -2,10 +2,10 @@ import { deleteCookie, getCookies } from "$std/http/cookie.ts";
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-  GET(_req: Request, ctx: HandlerContext) {
-    const url = new URL(_req.url);
-    const headers = new Headers(_req.headers);
-    deleteCookie(headers, "authToken", /*{ path: "/", domain: url.hostname }*/);
+  GET(req: Request, ctx: HandlerContext) {
+    const url = new URL(req.url);
+    const headers = new Headers(req.headers);
+    deleteCookie(headers, "authToken" /*{ path: "/", domain: url.hostname }*/);
     headers.set("location", "/signin");
     return new Response(null, {
       status: 302,
