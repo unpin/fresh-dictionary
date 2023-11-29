c027326bfb5ce8f93af12a76bbf09b48f937f772
German <pinncho@gmail.com>
2023-11-29 23:41:57 +0100

    feat: implement menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,9 @@
+import MenuOverlay from "../islands/MenuOverlay.tsx";
+
 export default function Navbar() {
   return (
     <header class="header">
-      <a href="/">
-        <div>Words</div>
-      </a>
-      <a href="/bookmarks">Bookmarks</a>
-      <a href="/signout">Sign out</a>
+      <MenuOverlay />
     </header>
   );
 }
