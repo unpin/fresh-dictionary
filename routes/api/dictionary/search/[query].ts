19176a55494d843c9fc2d58c1b0af7908c57762c
German <pinncho@gmail.com>
2023-11-30 15:08:18 +0100

    feat: utilize mongodb text index for queries

--- routes/api/dictionary/search/[query].ts
+++ routes/api/dictionary/search/[query].ts
@@ -4,10 +4,10 @@ import { DictionaryEntry } from "../../../../models/DictionaryEntry.ts";
 export const handler: Handlers = {
   async GET(_req, ctx) {
     const query = ctx.params.query;
-    const escapedQuery = query.replaceAll(/[\(\)]/g, ".*");
-    const pattern = new RegExp(`^${escapedQuery}`, "i");
+    // const escapedQuery = query.replaceAll(/[\(\)]/g, ".*");
+    // const pattern = new RegExp(`^${escapedQuery}`, "i");
     const entries = await DictionaryEntry.findMany({
-      word: { $regex: pattern },
+      $text: { $search: query },
     }, { limit: 15 });
     console.log(entries[0]);
     return new Response(JSON.stringify(entries), {
