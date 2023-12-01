e379b7765318d9ac94eee16e697a01f1f59ff84b
German <pinncho@gmail.com>
2023-12-01 17:58:13 +0100

    feat: increase search history size to 8

--- islands/SaveWordToLocalStorage.tsx
+++ islands/SaveWordToLocalStorage.tsx
@@ -15,7 +15,7 @@ export default function SaveWordToLocalStorage(
     ) as SearchHistoryData[];
     if (!history.find((e) => e._id === _id)) {
       history.push({ _id, word });
-      storage.setItem("searchHistory", JSON.stringify(history.slice(-5)));
+      storage.setItem("searchHistory", JSON.stringify(history.slice(-8)));
     }
   }, []);
   return <></>;
