042e9497b3e2d654bc77a2833da9df04d80e879d
German <pinncho@gmail.com>
2023-12-01 15:56:05 +0100

    feat: use logo with a higher resolution (192x192)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <header class="header">
       <a href="/">
-        <img class="logo" src="/icons/48x48.png" alt="" />
+        <img class="logo" src="/icons/192x192.png" alt="" />
       </a>
     </header>
   );
