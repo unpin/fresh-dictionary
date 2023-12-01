fb78312b6a669960622649d02604fec66ee6a39c
German <pinncho@gmail.com>
2023-12-01 15:34:52 +0100

    feat: add navbar logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 export default function Navbar() {
   return (
     <header class="header">
-      <a href="/" class="logo">Wortschatz</a>
+      <a href="/">
+        <img class="logo" src="/icons/48x48.png" alt="" />
+      </a>
     </header>
   );
 }
