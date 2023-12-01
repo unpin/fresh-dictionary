2f464df1f1d1e9ab4775a1df331ffe047fca54f3
German <pinncho@gmail.com>
2023-12-01 17:54:38 +0100

    feat: move search history code to useEffect

--- islands/SaveWordToLocalStorage.tsx
+++ islands/SaveWordToLocalStorage.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "preact/hooks";
+
 interface SearchHistoryData {
   _id: string;
   word: string;
@@ -6,13 +8,15 @@ interface SearchHistoryData {
 export default function SaveWordToLocalStorage(
   { _id, word }: SearchHistoryData,
 ) {
-  const storage = localStorage;
-  const history = JSON.parse(
-    storage.getItem("searchHistory") ?? "[]",
-  ) as SearchHistoryData[];
-  if (!history.find((e) => e._id === _id)) {
-    history.push({ _id, word });
-    storage.setItem("searchHistory", JSON.stringify(history.slice(-5)));
-  }
+  useEffect(() => {
+    const storage = localStorage;
+    const history = JSON.parse(
+      storage.getItem("searchHistory") ?? "[]",
+    ) as SearchHistoryData[];
+    if (!history.find((e) => e._id === _id)) {
+      history.push({ _id, word });
+      storage.setItem("searchHistory", JSON.stringify(history.slice(-5)));
+    }
+  }, []);
   return <></>;
 }
