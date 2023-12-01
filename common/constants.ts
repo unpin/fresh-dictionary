8c848583fca7daec1cb0d44e8e0fb89ffe231671
German <pinncho@gmail.com>
2023-12-01 18:09:38 +0100

    feat: use dotenv loadSync method

--- common/constants.ts
+++ common/constants.ts
@@ -1,8 +1,8 @@
-import { load } from "std/dotenv/mod.ts";
+import { loadSync } from "std/dotenv/mod.ts";
 
 const isProd = Deno.env.get("env")?.toUpperCase() === "PRODUCTION";
 
 export const { DATABASE_URL, PORT, JWT_SECRET, LOG_LEVEL, TEST } = isProd
   ? Deno.env
     .toObject()
-  : await load();
+  : loadSync();
