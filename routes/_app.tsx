6833062edca8f3cbc4e06afe9d49010e9f50dabc
German <pinncho@gmail.com>
2023-12-02 17:04:15 +0100

    chore: wrap components in body

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -14,7 +14,9 @@ export default function App({ Component }: PageProps) {
         />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <Component />
+      <body>
+        <Component />
+      </body>
     </>
   );
 }
