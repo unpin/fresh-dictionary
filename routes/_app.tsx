39b62840780c234ea1eb4916e1393ac91b5e6e21
German <pinncho@gmail.com>
2023-12-03 20:01:26 +0100

    feat: add navigation menu

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -14,8 +14,34 @@ export default function App({ Component }: PageProps) {
         />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <body>
-        <Component />
+      <body class="main">
+        <div>
+          <Component />
+        </div>
+        <footer>
+          <ul class="navigation-menu">
+            <li>
+              <a href="/">
+                <img src="/icons/house-blank.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/icons/magnifying-glass.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/icons/lines-leaning.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="/bookmarks">
+                <img src="/icons/bookmark-light.svg" alt="" />
+              </a>
+            </li>
+          </ul>
+        </footer>
       </body>
     </>
   );
