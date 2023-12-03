d8db4157d9b643592faa65fbe36703ac0a741144
German <pinncho@gmail.com>
2023-12-03 22:02:11 +0100

    fix: remove bottom padding for header

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -15,9 +15,7 @@ export default function App({ Component }: PageProps) {
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body class="main">
-        <div>
-          <Component />
-        </div>
+        <Component />
         <footer class="navigation-menu">
           <ul>
             <li>
