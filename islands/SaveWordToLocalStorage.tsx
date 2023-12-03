fbc035e633853cdb0acd16118a4c05416c0209b9
German <42736230+unpin@users.noreply.github.com>
2023-12-03 23:06:57 +0100

    fest: increase history size to 10

--- islands/SaveWordToLocalStorage.tsx
+++ islands/SaveWordToLocalStorage.tsx
@@ -14,7 +14,7 @@ export default function SaveWordToLocalStorage(
     ) as SearchHistoryData[];
     const filtered = history.filter((e) => e._id !== _id);
     filtered.unshift({ _id, word });
-    localStorage.setItem("searchHistory", JSON.stringify(filtered.slice(0, 5)));
+    localStorage.setItem("searchHistory", JSON.stringify(filtered.slice(0, 10)));
   }, []);
   return <></>;
 }
