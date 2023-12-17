830d45c2598c36d8980e50b9978e0996943121c6
German <pinncho@gmail.com>
2023-12-17 23:20:11 +0100

    feat: group word definitions by type

--- islands/dictionary/DictionaryWord.tsx
+++ islands/dictionary/DictionaryWord.tsx
@@ -1,5 +1,8 @@
 import { useState } from "preact/hooks";
-import { WordEntry } from "../../models/DictionaryEntry.ts";
+import {
+  WordEntry,
+  WordEntryDefinition,
+} from "../../models/DictionaryEntry.ts";
 import BookmarkEntry from "../BookmarkEntry.tsx";
 import TTS from "../TTS.tsx";
 
@@ -9,11 +12,27 @@ interface DictionaryWordProps {
 
 export default function DictionaryWord({ entry }: DictionaryWordProps) {
   const [word, setEntry] = useState(entry);
+  // TODO delete example from Database
   const deleteExample = (defIdx: number, exampleIdx: number) => {
     word.definitions[defIdx].examples.splice(exampleIdx, 1);
     setEntry({ ...word });
   };
   const textToSpeak = word.article ? word.article + " " + word.word : word.word;
+
+  const groupByType = (entry: WordEntry) => {
+    return entry.definitions.reduce((prev, curr) => {
+      if (prev.has(curr.type)) {
+        prev.get(curr.type).push(curr);
+      } else {
+        prev.set(curr.type, [curr]);
+      }
+      return prev;
+    }, new Map()) as Map<string, WordEntryDefinition[]>;
+  };
+
+  const grouped = groupByType(word);
+  console.log(grouped);
+
   return (
     <>
       <div class="dictionary-heading">
@@ -21,31 +40,40 @@ export default function DictionaryWord({ entry }: DictionaryWordProps) {
         <TTS text={textToSpeak} />
         <BookmarkEntry wordId={word._id.toString()} />
       </div>
-      <span class="entry-type">{/* TODO ADD TYPE */}</span>
-      <h3 class="my-4">Bedeutungen</h3>
-      <ul class="my-4">
-        {word.definitions.map((def, defIdx) => {
-          return (
-            <li class="my-4">
-              <div class="definition-counter my-2">{defIdx + 1}.</div>
-              <div>{def.definition}</div>
-              {def.examples &&
-                (
-                  <ul class="definition-examples">
-                    {def.examples.map((e, exIdx) => (
-                      <li>
-                        {e}{" "}
-                        <span onClick={() => deleteExample(defIdx, exIdx)}>
-                          delete
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-            </li>
-          );
-        })}
-      </ul>
+      {Array.from(grouped.keys()).map((key) => {
+        return (
+          <>
+            <span class="entry-type">{key}</span>
+            <ul class="my-4">
+              {grouped.get(key)!.map(
+                (def: WordEntryDefinition, defIdx: number) => {
+                  return (
+                    <li class="my-4">
+                      <div class="definition-counter my-2">{defIdx + 1}.</div>
+                      <div>{def.definition}</div>
+                      {def.examples &&
+                        (
+                          <ul class="definition-examples">
+                            {def.examples.map((e, exIdx) => (
+                              <li>
+                                {e}{" "}
+                                <span
+                                  onClick={() => deleteExample(defIdx, exIdx)}
+                                >
+                                  delete
+                                </span>
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                    </li>
+                  );
+                },
+              )}
+            </ul>
+          </>
+        );
+      })}
     </>
   );
 }
