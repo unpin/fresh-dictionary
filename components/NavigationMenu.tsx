41045ace6bdbed7776546370f5c525fdc66afa51
German <pinncho@gmail.com>
2023-12-29 22:37:54 +0100

    style: wrap navigation-menu items in container

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -3,28 +3,30 @@ import Icon from "./Icon.tsx";
 export default function NavigationMenu() {
   return (
     <footer class="navigation-menu">
-      <ul>
-        <li>
-          <a href="/">
-            <Icon name="house-blank" />
-          </a>
-        </li>
-        <li>
-          <a href="/bookmarks">
-            <Icon name="bookmark" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Icon name="magnifying-glass" />
-          </a>
-        </li>
-        <li>
-          <a href="/profile">
-            <Icon name="user" />
-          </a>
-        </li>
-      </ul>
+      <div class="container">
+        <ul>
+          <li>
+            <a href="/">
+              <Icon name="house-blank" />
+            </a>
+          </li>
+          <li>
+            <a href="/bookmarks">
+              <Icon name="bookmark" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Icon name="magnifying-glass" />
+            </a>
+          </li>
+          <li>
+            <a href="/profile">
+              <Icon name="user" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
