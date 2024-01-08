e91196e58f8f60a94f1a13bfa6b3c75996346ff1
German <pinncho@gmail.com>
2024-01-08 21:03:07 +0100

    feat: add route for updating a definition

--- routes/api/dictionary/[_id]/definition/[definitionId]/index.ts
+++ routes/api/dictionary/[_id]/definition/[definitionId]/index.ts
@@ -3,6 +3,29 @@ import { ObjectId } from "mongo";
 import { DictionaryEntry } from "../../../../../../models/DictionaryEntry.ts";
 
 export const handler: Handlers = {
+  async PATCH(req, ctx) {
+    const { _id, definitionId } = ctx.params;
+    const { definition, examples } = await req.json();
+    console.log({ definition, examples });
+
+    const { matchedCount } = await DictionaryEntry.updateOne({
+      _id: new ObjectId(_id),
+      "definitions._id": new ObjectId(definitionId),
+    }, {
+      $set: {
+        "definitions.$.definition": definition,
+        "definitions.$.examples": examples,
+      },
+    });
+    if (matchedCount) {
+      return new Response(null, {
+        status: 200,
+      });
+    }
+    return new Response(null, {
+      status: 400,
+    });
+  },
   async DELETE(_req, ctx) {
     const { _id, definitionId } = ctx.params;
     const { modifiedCount } = await DictionaryEntry.updateOne({
