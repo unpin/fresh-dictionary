8a9b7b744b09af01dcbefeca031f4ef55a4d8e46
German <pinncho@gmail.com>
2024-01-08 21:01:57 +0100

    refactor: add interfaces UserAuth and AuthCookie

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -3,20 +3,39 @@ import { useEffect } from "preact/hooks";
 import { parseJWT } from "../services/JWTService.ts";
 import { getCookie } from "../services/ClientService.ts";
 
-export function useAuth(): [Record<string, unknown> | null] {
-  const auth = useSignal<Record<string, unknown> | null>({
+interface UserAuth {
+  isLogged: boolean;
+  isAdmin: boolean;
+  _id: string;
+  name: string;
+  email: string;
+}
+
+interface AuthCookie {
+  _id: string;
+  email: string;
+  name: string;
+  userRole: string;
+}
+
+export function useAuth(): [UserAuth] {
+  const auth = useSignal<UserAuth>({
     isLogged: false,
     isAdmin: false,
+    _id: "",
+    name: "",
+    email: "",
   });
 
   useEffect(() => {
     const cookie = getCookie("authToken");
     if (!cookie) return;
-    const authToken = parseJWT(cookie);
+    const authToken: AuthCookie = parseJWT(cookie);
     if (!authToken) return;
     auth.value = {
       isLogged: true,
       isAdmin: authToken.userRole === "admin",
+      _id: authToken._id,
       name: authToken.name,
       email: authToken.email,
     };
