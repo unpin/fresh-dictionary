e26c7126e8444449dc173eb77e4aa053d598b82c
German <pinncho@gmail.com>
2024-01-08 21:20:26 +0100

    feat: allow edit definitions only for users with role admin

--- islands/dictionary/DictionaryDefinition.tsx
+++ islands/dictionary/DictionaryDefinition.tsx
@@ -2,6 +2,7 @@ import { StateUpdater, useEffect, useState } from "preact/hooks";
 import Icon from "../../components/Icon.tsx";
 import { Definition, Word } from "../../types/words.ts";
 import { JSX } from "preact/jsx-runtime";
+import { useAuth } from "../../hooks/useAuth.tsx";
 
 interface DictionaryDefinitionProps {
   definition: Definition;
@@ -15,8 +16,13 @@ export default function DictionaryDefinition(
   { definition, order, setWord, onDeleteDefinition, wordId }:
     DictionaryDefinitionProps,
 ) {
+  const [def, setDef] = useState(definition.definition);
   const [isContentEditable, setIsContentEditable] = useState(false);
   const [examples, setExamples] = useState<string[]>([]);
+  const [auth] = useAuth();
+
+  const handleAddMeaning = () => {
+  };
 
   useEffect(() => {
     if (definition.examples) {
@@ -24,29 +30,50 @@ export default function DictionaryDefinition(
     }
   }, [definition]);
 
+  const handleUpdateDefinition = () => {
+  };
+
+  const handleDeleteMeaning = () => {
+  };
+
   return (
     <li>
       <div class="definition-counter my-2">{order + 1}.</div>
       <div class="definition-term">
-        <p contentEditable={isContentEditable}>{definition.definition}</p>
-        {!isContentEditable && (
-          <Icon
-            name="pen-to-square"
-            onClick={() => setIsContentEditable(!isContentEditable)}
-          />
+        <input
+          class="form-input"
+          disabled={!isContentEditable}
+          value={def}
+          onChange={(e) => {
+            const inputField = e.target as HTMLInputElement;
+            setDef(inputField.value);
+          }}
+        >
+        </input>
+        {auth.isAdmin && (
+          <>
+            {!isContentEditable && (
+              <Icon
+                name="pen-to-square"
+                onClick={() => setIsContentEditable(!isContentEditable)}
+              />
+            )}
+          </>
         )}
       </div>
       <ul class="definition-examples">
-        {examples.map((example, idx) => (
+        {examples.map((example) => (
           <li>
-            <p contentEditable={isContentEditable}>
-              {example}
-            </p>
+            {example}
           </li>
         ))}
       </ul>
       {isContentEditable && (
         <div class="definition-buttons">
+          <button class="btn" onClick={handleAddMeaning}>
+            <Icon name="circle-plus" />
+            Add new meaning
+          </button>
           <button
             class="btn"
             data-_id={definition._id}
@@ -54,6 +81,10 @@ export default function DictionaryDefinition(
           >
             <Icon name="trash" />
             Delete Definition
+          </button>
+          <button class="btn" onClick={handleUpdateDefinition}>
+            <Icon name="circle-check" />
+            Save
           </button>
         </div>
       )}
