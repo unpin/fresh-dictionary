f001b33b2dcc7b190b6e8ef17d210ab0b930fe7f
German B. <pinncho@gmail.com>
2024-01-30 21:19:23 +0100

    feat: add title to bookmarks page

--- routes/bookmarks/index.tsx
+++ routes/bookmarks/index.tsx
@@ -1,10 +1,14 @@
 import BookmarkList from "../../islands/bookmarks/BookmarkList.tsx";
 import Header from "../../components/Header.tsx";
 import NavbarSearch from "../../islands/NavbarSearch.tsx";
+import { Head } from "$fresh/runtime.ts";
 
 export default function Bookmarks() {
   return (
     <>
+      <Head>
+        <title>Bookmarks | Words</title>
+      </Head>
       <Header>
         <NavbarSearch />
       </Header>
