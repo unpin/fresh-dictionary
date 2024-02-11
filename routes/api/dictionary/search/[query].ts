366285864c415dff082fba43a1937217f3c37783
German B. <pinncho@gmail.com>
2024-02-11 18:00:57 +0100

    feat: use regex for searching dictionary entries

--- routes/api/dictionary/search/[query].ts
+++ routes/api/dictionary/search/[query].ts
@@ -4,22 +4,24 @@ import { DictionaryEntry } from "../../../../models/DictionaryEntry.ts";
 export const handler: Handlers = {
   async GET(_req, ctx) {
     const query = ctx.params.query;
-    // const escapedQuery = query.replaceAll(/[\(\)]/g, ".*");
-    // const pattern = new RegExp(`^${escapedQuery}`, "i");
-    const decodedQuery = decodeURI(query);
-    const entries = await DictionaryEntry.findMany({
-      $text: {
-        $search: decodedQuery,
-        $caseSensitive: false,
-        $diacriticSensitive: false,
-      },
-    }, {
-      projection: {
-        word: 1,
-        article: 1,
-      },
-      limit: 50,
-    });
+    const escapedQuery = query.replaceAll(/[\(\)]/g, ".*");
+    const pattern = new RegExp(`^${escapedQuery}`, "i");
+    // const decodedQuery = decodeURI(query);
+    // const entries = await DictionaryEntry.findMany({
+    //   $text: {
+    //     $search: decodedQuery,
+    //     $caseSensitive: false,
+    //     $diacriticSensitive: false,
+    //   },
+    // }, {
+    //   projection: {
+    //     word: 1,
+    //     article: 1,
+    //   },
+    //   limit: 50,
+    // });
+
+    const entries = await DictionaryEntry.findMany({ word: pattern });
 
     return new Response(JSON.stringify(entries), {
       status: 200,
