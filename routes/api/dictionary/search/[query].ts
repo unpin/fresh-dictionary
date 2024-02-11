c7d56b93fb26be73c01627724ba3ad32789b4ecd
German B. <pinncho@gmail.com>
2024-02-11 18:06:40 +0100

    revert: use text search for better query results and temporarely disable limit

--- routes/api/dictionary/search/[query].ts
+++ routes/api/dictionary/search/[query].ts
@@ -4,24 +4,22 @@ import { DictionaryEntry } from "../../../../models/DictionaryEntry.ts";
 export const handler: Handlers = {
   async GET(_req, ctx) {
     const query = ctx.params.query;
-    const escapedQuery = query.replaceAll(/[\(\)]/g, ".*");
-    const pattern = new RegExp(`^${escapedQuery}`, "i");
-    // const decodedQuery = decodeURI(query);
-    // const entries = await DictionaryEntry.findMany({
-    //   $text: {
-    //     $search: decodedQuery,
-    //     $caseSensitive: false,
-    //     $diacriticSensitive: false,
-    //   },
-    // }, {
-    //   projection: {
-    //     word: 1,
-    //     article: 1,
-    //   },
-    //   limit: 50,
-    // });
-
-    const entries = await DictionaryEntry.findMany({ word: pattern });
+    // const escapedQuery = query.replaceAll(/[\(\)]/g, ".*");
+    // const pattern = new RegExp(`^${escapedQuery}`, "i");
+    const decodedQuery = decodeURI(query);
+    const entries = await DictionaryEntry.findMany({
+      $text: {
+        $search: decodedQuery,
+        $caseSensitive: false,
+        $diacriticSensitive: false,
+      },
+    }, {
+      projection: {
+        word: 1,
+        article: 1,
+      },
+      // limit: 50,
+    });
 
     return new Response(JSON.stringify(entries), {
       status: 200,
