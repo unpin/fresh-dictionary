1550734e145e530bb84b7ba9152c7447837e0977
German B. <engwithmusic@gmail.com>
2024-03-06 17:35:19 +0100

    style: update navbar search styles and add animation

--- islands/NavbarSearch.tsx
+++ islands/NavbarSearch.tsx
@@ -15,6 +15,7 @@ const queryWordsThrottled = asyncThrottle(queryWords, 500);
 
 export default function NavbarSearch() {
   const searchFieldRef = useRef<HTMLInputElement>(null);
+  const searchContentRef = useRef<HTMLDivElement>(null);
   const [query, setQuery] = useState("");
   const [entries, setEntries] = useState<Word[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -47,10 +48,16 @@ export default function NavbarSearch() {
   };
 
   const closeSearchContent = () => {
-    setShowContent(false);
-    setEntries([]);
-    if (body.current) {
-      body.current.style.overflow = "auto";
+    if (searchContentRef.current) {
+      searchContentRef.current.classList.add("close");
+      setTimeout(() => {
+        searchContentRef.current!.classList.remove("close");
+        setShowContent(false);
+        setEntries([]);
+        if (body.current) {
+          body.current.style.overflow = "auto";
+        }
+      }, 300);
     }
   };
 
@@ -99,60 +106,74 @@ export default function NavbarSearch() {
       </div>
       {showContent && (
         <>
-          <div class="search-content" onClick={() => closeSearchContent()}>
+          <div
+            class="search-content"
+            ref={searchContentRef}
+            onClick={() => closeSearchContent()}
+          >
             <div class="container">
               {query.length > 0 && entries.length > 0
                 ? (
-                  <ul class="items">
-                    {entries.map(({ _id, article, word }) => (
-                      <li class="item">
-                        <a
-                          href={"/dictionary/" + encodeURI(_id)}
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            addSearchItem({
-                              _id,
-                              searchTerm: article + " " + word,
-                            });
-                          }}
-                        >
-                          {article
-                            ? <span class="search-article">{article}</span>
-                            : ""} {word}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                )
-                : (
-                  <ul class="items">
-                    {searchItems.map(({ _id, searchTerm }) => (
-                      <li class="item">
-                        <div class="history-entry">
-                          <div class="history-entry-word">
-                            <ClockRotateLeft class="icon" />
-                            <a
-                              href={"/dictionary/" + _id}
-                              onClick={(e) => {
-                                e.stopPropagation();
-                              }}
-                            >
-                              {searchTerm}
-                            </a>
-                          </div>
-                          <div
-                            class="history-close-btn"
+                  <div class="search-items">
+                    <ul class="items">
+                      {entries.map(({ _id, article, word }) => (
+                        <li class="item">
+                          <a
+                            href={"/dictionary/" + encodeURI(_id)}
                             onClick={(e) => {
                               e.stopPropagation();
-                              deleteSearchItem(_id);
+                              addSearchItem({
+                                _id,
+                                searchTerm: article + " " + word,
+                              });
                             }}
                           >
-                            <Xmark class="icon" />
+                            {article
+                              ? <span class="search-article">{article}</span>
+                              : ""} {word}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                    <footer>
+                      <a href="">Load more</a>
+                    </footer>
+                  </div>
+                )
+                : (
+                  <div class="history-items">
+                    <ul class="items">
+                      {searchItems.map(({ _id, searchTerm }) => (
+                        <li class="item">
+                          <div class="history-entry">
+                            <div class="flex gap">
+                              <ClockRotateLeft class="icon" />
+                              <a
+                                href={"/dictionary/" + _id}
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                }}
+                              >
+                                {searchTerm}
+                              </a>
+                            </div>
+                            <div
+                              class="btn-delete"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                deleteSearchItem(_id);
+                              }}
+                            >
+                              <Xmark class="icon" />
+                            </div>
                           </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
+                        </li>
+                      ))}
+                    </ul>
+                    <footer>
+                      <a href="">Alle löschen</a>
+                    </footer>
+                  </div>
                 )}
             </div>
           </div>
