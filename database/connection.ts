5b2d57cc2dba871cb7d6683ff60509e399401881
German B. <pinncho@gmail.com>
2024-04-15 17:58:42 +0200

    Revert "feat: Deno.exit(1) on connection error"
    This reverts commit 98f72f71efaa0044b9f60fc1dcd3467b8cdc52d8.

--- database/connection.ts
+++ database/connection.ts
@@ -3,11 +3,7 @@ import { DATABASE_URL } from "../common/constants.ts";
 
 async function connect(): Promise<Database> {
   const client = new MongoClient();
-  try {
-    return await client.connect(DATABASE_URL);
-  } catch {
-    Deno.exit(1);
-  }
+  return await client.connect(DATABASE_URL);
 }
 
 export const conn = await connect();
