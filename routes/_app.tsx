dcce7bd94a613e0418b822f65b22765566ebf4a3
German <pinncho@gmail.com>
2024-09-21 01:16:14 +0200

    style: import Inter font weight 700

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component }: PageProps) {
           crossorigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
         <link rel="manifest" href="/manifest.json" />
