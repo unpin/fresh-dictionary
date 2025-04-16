d19e70574426ae0113f11110a3c11a737b643204
German Bilalov <pinncho@gmail.com>
2025-04-16 21:07:36 +0200

    style: set theme-color for safari

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component }: PageProps) {
   return (
     <>
       <Head>
+        <meta name="theme-color" content="#1c1c1e" />
         <link rel="stylesheet" href="/styles.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
